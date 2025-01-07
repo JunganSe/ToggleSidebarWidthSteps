5f5da12241f4c59a1ef715742e7843764da44ee0
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-01-07 21:49:57 +0100

    Implement toggling of sidebar width

--- src/manager.ts
+++ src/manager.ts
@@ -1,9 +1,11 @@
 import * as vscode from 'vscode';
+import { DECREASE_TO_MINIMUM_LOOPCOUNT, KEY_WIDTH_A, KEY_WIDTH_B } from './constants';
 
 let self: SidebarWidthManager;
 
 export class SidebarWidthManager {
     private readonly context: vscode.ExtensionContext;
+    private aWasLast: boolean = false;
 
     constructor(context: vscode.ExtensionContext) {
         self = this;
@@ -12,16 +14,50 @@ export class SidebarWidthManager {
 
     toggleWidth() {
         console.log('toggleWidth');
-        // TODO: Implement toggleWidth.
+
+        if (self.aWasLast)
+            self.applyWidthB();
+        else
+            self.applyWidthA();
     }
 
     applyWidthA() {
         console.log('applyWidthA');
-        // TODO: Implement applyWidthA.
+        const widthA = self.getConfigNumber(KEY_WIDTH_A);
+        self.setWidth(widthA);
+        self.aWasLast = true;
     }
 
     applyWidthB() {
         console.log('applyWidthB');
-        // TODO: Implement applyWidthB.
+        const widthB = self.getConfigNumber(KEY_WIDTH_B);
+        self.setWidth(widthB);
+        self.aWasLast = false;
+    }
+
+    private getConfigNumber(key: string) {
+        const value = vscode.workspace
+            .getConfiguration()
+            .get<number>(key);
+        return Number(value);
+    }
+
+    private setWidth(steps: number) {
+        vscode.commands.executeCommand('workbench.action.focusSideBar');
+        self.decreaseWithToMinimum();
+        self.increaseWidthBySteps(steps);
+        vscode.commands.executeCommand('workbench.action.focusLastEditorGroup');
+    }
+
+    private decreaseWithToMinimum() {
+        for (let i = 0; i < DECREASE_TO_MINIMUM_LOOPCOUNT; i++) {
+            vscode.commands.executeCommand('workbench.action.decreaseViewSize');
+        }
+    }
+
+    private increaseWidthBySteps(steps: number) {
+        for (let i = 0; i < steps; i++) {
+            vscode.commands.executeCommand('workbench.action.increaseViewSize');
+        }
     }
 }

d98fc42f32c5c07a1d4ff940fc755f350ad49271
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-01-07 21:37:33 +0100

    Add constants

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+// Configuration keys
+export const KEY_WIDTH_A = 'toggleSidebarWidth.widthA';
+export const KEY_WIDTH_B = 'toggleSidebarWidth.widthB';
+
+// Magic numbers
+export const DECREASE_TO_MINIMUM_LOOPCOUNT = 20;
